e55c5dd22241dbe2da75d21b3c4247bb642af278
AlexanderFraltsov <kolonessens@yandex.ru>
2024-02-03 19:28:35 +0400

    refactor: read file

--- modules/files-operations-dispatcher/operations/read-file.mjs
+++ modules/files-operations-dispatcher/operations/read-file.mjs
@@ -1,6 +1,7 @@
+import { resolve } from 'node:path';
+import { realpath } from 'node:fs/promises';
 import { createReadStream } from 'node:fs';
 
-import { getAbsolutePath } from '../../event-dispatcher/operations/get-absolute-path.mjs';
 import { ERROR_OPERATION_FAILED } from '../../../constants/constants.js';
 
 /**
@@ -8,13 +9,18 @@ import { ERROR_OPERATION_FAILED } from '../../../constants/constants.js';
  * @param {string} workingDirectoryPath
  */
 export const readFile = async (filename, workingDirectoryPath) => {
-	const path = await getAbsolutePath(workingDirectoryPath, filename);
-	return new Promise(res => createReadStream(path, { encoding: 'utf-8' })
-		.on('data', (data) => console.log(data))
-		.on('error', () => {
-			console.log(ERROR_OPERATION_FAILED);
-			res();
-		})
-		.on('close', () => res()),
-	);
+	try {
+		const path = resolve(workingDirectoryPath, filename);
+		await realpath(path);
+		return new Promise(res => createReadStream(path, { encoding: 'utf-8' })
+			.on('data', (data) => console.log(data))
+			.on('error', () => {
+				console.log(ERROR_OPERATION_FAILED);
+				res();
+			})
+			.on('close', () => res()),
+		);
+	} catch {
+		throw new Error(ERROR_OPERATION_FAILED);
+	}
 }
